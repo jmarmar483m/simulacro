a74eff32f51c2209653909aed088c2c29603ae46
Juan Martinez <juanmmcontacto@gmail.com>
2024-04-28 17:29:24 +0200

    javadoc creado

--- src/vehiculo/Main.java
+++ src/vehiculo/Main.java
@@ -1,44 +1,43 @@
-
 package vehiculo;
 
 /**
- *
- * @author 
+ * 
+ * @author Juan Martinez
  */
 public class Main {
-    
-      public static void main(String[] args) {
+/**
+ * 
+ * @param args 
+ */
+    public static void main(String[] args) {
         VehiculoXXX2224 miVehiculoXXX2224;
         int stockActual;
-        
-        miVehiculoXXX2224 = new VehiculoXXX2224("Seat",18000,100);
-        
-        operativaVehiculosXXX22224(miVehiculoXXX2224, 50); 
-        
+
+        miVehiculoXXX2224 = new VehiculoXXX2224("Seat", 18000, 100);
+
+
+        operativaVehiculosXXX22224(miVehiculoXXX2224, 50);
+
         stockActual = miVehiculoXXX2224.obtenerStock();
-        
-        System.out.println("El stock actual es "+ stockActual );
+
+        System.out.println("El stock actual es " + stockActual);
     }
 
     private static void operativaVehiculosXXX22224(VehiculoXXX2224 miVehiculoXXX2224, int cantidad) {
-        try
-        {
+        try {
             System.out.println("Venta de Vehiculos");
             miVehiculoXXX2224.vender(20);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.print("Fallo al vender");
         }
-        
-        try
-        {
+        try {
             System.out.println("Compra de Vehiculos");
             miVehiculoXXX2224.comprar(100);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.print("Fallo al comprar");
         }
+
     }
 
+
 }
-    
--- src/vehiculo/VehiculoXXX2224.java
+++ src/vehiculo/VehiculoXXX2224.java
@@ -1,123 +1,125 @@
-
 package vehiculo;
-
 /**
  *
- * @author 
+ * @author Juan Martinez
  */
 public class VehiculoXXX2224 {
-
     /**
-     * @return the nombre
+     * @return nombre devuelve el nombre del vehiculo
      */
     public String getNombre() {
         return nombre;
     }
-
     /**
-     * @param nombre the nombre to set
+     * @param nombre introduce el nombre del vehiculo
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     /**
-     * @return the precio
+     * @return precio devuelve el precio
      */
     public double getPrecio() {
         return precio;
     }
-
     /**
-     * @param precio the precio to set
+     * @param precio introduce el precio del vehiculo
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
-
     /**
-     * @return the precioIVA
+     * @return precioIVA el precio con IVA
      */
     public double getPrecioIVA() {
         return precioIVA;
     }
-
     /**
-     * @param precioIVA the precioIVA to set
+     * @param precioIVA introduce el precio ya con IVA
      */
     public void setPrecioIVA(double precioIVA) {
         this.precioIVA = precioIVA;
     }
-
     /**
-     * @return the stock
+     * @return the stock cantidad de producto
      */
     public int getStock() {
         return stock;
     }
-
     /**
-     * @param stock the stock to set
+     * @param stock marca la cantidad que hay del producto
      */
     public void setStock(int stock) {
         this.stock = stock;
     }
-
+    /*atributos*/
     private String nombre;
     private double precio;
     private double precioIVA;
     private int stock;
-
     /* Constructor sin argumentos */
-    public VehiculoXXX2224 ()
-    {
+    public VehiculoXXX2224() {
     }
     // Constructor con parámetro para iniciar todas las propiedades de la clase
-    
-    
-    public VehiculoXXX2224 (String nom, double precio, int stock)
-    {
-        this.nombre =nom;
-        this.precio=precio;
-        this.stock=stock;
+    /**
+     * @param nom nombre
+     * @param precio precio
+     * @param stock cantidad en stock
+     */
+    public VehiculoXXX2224(String nom, double precio, int stock) {
+        this.nombre = nom;
+        this.precio = precio;
+        this.stock = stock;
     }
-   // Método para asignar el nombre del vehiculo
-    public void asignarNombre(String nom)
-    {
+    // Método para asignar el nombre del vehiculo
+    /**
+     *
+     * @param nom asigna el nombre al vehiculo
+     */
+    public void asignarNombre(String nom) {
         setNombre(nom);
     }
     // Método que me devuelve el nombre del vehiculo
-    public String obtenerNombre()
-    {
+    /**
+     *
+     * @return devuelve el nombre del vehiculo
+     */
+    public String obtenerNombre() {
         return getNombre();
     }
-
     // Método que me devuelve el stock de vehiculos disponible en cada momento
-     public int obtenerStock ()
-    {
+    /**
+     *
+     * @return Stock la cantidad de producto
+     */
+    public int obtenerStock() {
         return getStock();
     }
-
     /* Método para comprar vehiculos. Modifica el stock.
      * Este método va a ser probado con Junit
      */
-    public void comprar(int cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     *
+     * @param cantidad numero de vehiculos a comprar
+     * @throws Exception excepcion en caso de no tener stock
+     */
+    public void comprar(int cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede comprar un nº negativo de vehiculos");
-        setStock(getStock() + cantidad);
+        }        setStock(getStock() + cantidad);
+    }
+    /**
+     *
+     * @param cantidad cantidad de vehiculos a vender
+     * @throws Exception en caso de que se vendan más vehiculos que los que hay
+     * en stock
+     */
+    public void vender(int cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede vender una cantidad negativa de vehiculos");
+        }        if (obtenerStock() < cantidad) {
+            throw new Exception("No se hay suficientes vehiculos para vender");
+        }        setStock(getStock() - cantidad);
     }
 
-    public void vender (int cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede vender una cantidad negativa de vehiculos");
-        if (obtenerStock()< cantidad)
-            throw new Exception ("No se hay suficientes vehiculos para vender");
-        setStock(getStock() - cantidad);
-    }
-    
-}  
-   
-    
-
+}
